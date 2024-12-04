1f7b2124675461058463187a8e42e4de6ebd2c8e
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2024-12-04 22:10:35 +0600

    vue setup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,7 +18,18 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'test@app.com',
+            'password' => Hash::make('12345678')
+        ]);
+        User::factory()->create([
+            'name' => 'admin User',
+            'email' => 'admin@app.com',
+            'password' => Hash::make('12345678')
+        ]);
+        User::factory()->create([
+            'name' => 'user User',
+            'email' => 'user@app.com',
+            'password' => Hash::make('12345678')
         ]);
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,18 @@
 import './bootstrap';
 
-import Alpine from 'alpinejs';
+// import Alpine from 'alpinejs';
 
-window.Alpine = Alpine;
+// window.Alpine = Alpine;
 
-Alpine.start();
+// Alpine.start();
+
+import { createApp } from "vue";
+import ExampleComponent from "./components/ExampleComponent.vue";
+
+const app = createApp({});
+
+// Register components
+app.component("example-component", ExampleComponent);
+
+// Mount the app
+app.mount("#app");
